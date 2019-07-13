4f8f8b37b4556d2441692a5a40f92aff1f75af71
jasmine <jazyan11@gmail.com>
2019-07-12 17:29:20 -0700

    add edge logic!

--- js/graph_drawer.js
+++ js/graph_drawer.js
@@ -1,3 +1,6 @@
+// TODO: delete a vertex, must delete edges
+// TODO: delete edge
+
 var canvas = document.getElementById("canvas");
 
 var ctx = canvas.getContext("2d");
@@ -23,7 +26,7 @@ function createSVGCircle(x, y, r, strokeColor) {
     circle.setAttribute("cx", x);
     circle.setAttribute("cy", y);
     circle.setAttribute("r", r);
-    circle.setAttribute("fill", "none");
+    circle.setAttribute("fill", "white");
     circle.setAttribute("stroke", strokeColor);
     circle.setAttribute("stroke-width", "2px");
     return circle;
@@ -36,8 +39,6 @@ function draw(event) {
     var pos = getMousePos(canvas, event);
     var circle = createSVGCircle(pos.x, pos.y, radius, "black");
     svg.appendChild(circle);
-    console.log(svg.childNodes);
-    console.log(svg.children);
 }
 
 // checks whether the click is in the boundary of a node
@@ -45,52 +46,81 @@ function draw(event) {
 // unselect --> need to change color back to black...
 function checkBoundary(event, distance) {
     var pos = getMousePos(canvas, event);
-    // first child of SVG is script, so start at index 1
-    if (svg.children.length < 2) {
-        return -1;
-    }
-    for (var i = 1; i < svg.children.length; ++i) {
-        var currCircle = svg.children[i];
-        var cx = currCircle.getAttribute("cx");
-        var cy = currCircle.getAttribute("cy");
+    for (var i = 0; i < svg.children.length; ++i) {
+        var currNode = svg.children[i];
+        // only consider SVG nodes that are circles
+        if (currNode.nodeName !== "circle") {
+            continue;
+        }
+        var cx = currNode.getAttribute("cx");
+        var cy = currNode.getAttribute("cy");
         var centerDist = Math.pow(pos.x - cx, 2) + Math.pow(pos.y - cy, 2);
         if (centerDist < Math.pow(distance, 2)) {
-            console.log("HELLO" + currCircle);
             return i;
         }
     }
     return -1;  // no matches
 }
 
-function deleteNode(event) {
+// deletes the current selectedNode
+function deleteNode() {
     if (selectedNode != null) {
         svg.removeChild(selectedNode);
         selectedNode = null;
     }
 }
 
+// taken from https://stackoverflow.com/a/6091752
+function computeBoundaryPoint(x1, y1, x2, y2) {
+    var theta = Math.atan2(y2 - y1, x2 - x1);
+    return [x1 + radius * Math.cos(theta), y1 + radius * Math.sin(theta)]
+}
+
+// given node centers (x1, y1) of circle C1 and (x2, y2) of circle C2
+// draw an edge from the boundary of C1 to the boundary of C2
+function drawEdge(x1, y1, x2, y2) {
+    var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    var bp1 = computeBoundaryPoint(x1, y1, x2, y2);
+    var bp2 = computeBoundaryPoint(x2, y2, x1, y1);
+    line.setAttribute("x1", bp1[0]);
+    line.setAttribute("y1", bp1[1]);
+    line.setAttribute("x2", bp2[0]);
+    line.setAttribute("y2", bp2[1]);
+    line.setAttribute("stroke-width", 1.5);
+    line.setAttribute("stroke", "black");
+    svg.appendChild(line);
+}
+
+// this is more like deselect / select logic
+// with drawing edge logic
+// TODO: find a better name
 function colorCircle(index) {
     var circle = svg.children[index];
-    var strokeColor = "black";
-    var currColor = circle.getAttribute("stroke");
-    if (currColor === "black") {
-        strokeColor = "red";
-        // check if there's currently a selected node
-        // and deselect it by changing the color back to black
-        if (selectedNode != null) {
-            selectedNode.setAttribute("stroke", "black"); 
-        }
+    if (selectedNode == null) {
+        // if there's no currently selected node, the circle
+        // becomes the node
+        circle.setAttribute("stroke", "red");
         selectedNode = circle;
+    } else if (selectedNode == circle) {
+        // if the currently selected node is the circle, deselect it
+        selectedNode.setAttribute("stroke", "black");
+        selectedNode = null;
     } else {
-        // current node is red, so it is currently selected. deselect it
+        // there is a selected node, and our current circle is another node
+        // that means we want to draw an edge! then deselect the node
+        x1 = parseInt(selectedNode.getAttribute("cx"));
+        y1 = parseInt(selectedNode.getAttribute("cy"));
+        x2 = parseInt(circle.getAttribute("cx"));
+        y2 = parseInt(circle.getAttribute("cy"));
+        drawEdge(x1, y1, x2, y2);
+        selectedNode.setAttribute("stroke", "black");
         selectedNode = null;
     }
-    circle.setAttribute("stroke", strokeColor);
 }
 
 document.onkeydown = function (e) {
     if (e.keyCode == 8) {  // backspace
-        deleteNode(e);
+        deleteNode();
     }
 }
 
