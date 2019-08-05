36f6cb8ce60049a76357aa2e21bdd21d05de53cb
jasmine <jazyan11@gmail.com>
2019-08-04 19:26:03 -0700

    clean up drag code

--- js/drag.js
+++ js/drag.js
@@ -1,46 +1,46 @@
 var mousedElement = null;
 
 function startDrag(node) {
-    console.log("start drag");
-    mousedElement = node;
+    if (node !== null && node.nodeName === "circle") {
+        mousedElement = node;
+    }
+}
+
+function updateEdge(x1, y1, x2, y2, edge) {
+    var bp1 = computeBoundaryPoint(x1, y1, x2, y2);
+    var bp2 = computeBoundaryPoint(x2, y2, x1, y1);
+    edge.setAttribute("x1", bp1[0]);
+    edge.setAttribute("y1", bp1[1]);
+    edge.setAttribute("x2", bp2[0]);
+    edge.setAttribute("y2", bp2[1]);
 }
 
 function drag(evt) {
     if (!mousedElement) {
         return;
     }
-    // TODO: assume that it is a node
-    console.log("dragging");
     evt.preventDefault();
     var pos = getMousePos(canvas, evt);
-    var edgesToModify = [];
     for (var i = 0; i < edgeList.length; ++i) {
         var node1 = edgeList[i][0];
         var node2 = edgeList[i][1];
         var edge = edgeList[i][2];
-        // TODO: condense below lol
         if (mousedElement === node1){
-            var x1 = parseInt(pos.x);
-            var y1 = parseInt(pos.y);
-            var x2 = parseInt(node2.getAttribute("cx"));
-            var y2 = parseInt(node2.getAttribute("cy"));
-            var bp1 = computeBoundaryPoint(x1, y1, x2, y2);
-            var bp2 = computeBoundaryPoint(x2, y2, x1, y1);
-            edge.setAttribute("x1", bp1[0]);
-            edge.setAttribute("y1", bp1[1]);
-            edge.setAttribute("x2", bp2[0]);
-            edge.setAttribute("y2", bp2[1]);
+            updateEdge(
+                parseInt(pos.x),
+                parseInt(pos.y),
+                parseInt(node2.getAttribute("cx")),
+                parseInt(node2.getAttribute("cy")),
+                edge
+            );
         } else if (mousedElement === node2) {
-            var x1 = parseInt(node1.getAttribute("cx"));
-            var y1 = parseInt(node1.getAttribute("cy"));
-            var x2 = parseInt(pos.x);
-            var y2 = parseInt(pos.y);
-            var bp1 = computeBoundaryPoint(x1, y1, x2, y2);
-            var bp2 = computeBoundaryPoint(x2, y2, x1, y1);
-            edge.setAttribute("x1", bp1[0]);
-            edge.setAttribute("y1", bp1[1]);
-            edge.setAttribute("x2", bp2[0]);
-            edge.setAttribute("y2", bp2[1]);
+            updateEdge(
+                parseInt(node1.getAttribute("cx")),
+                parseInt(node1.getAttribute("cy")),
+                parseInt(pos.x),
+                parseInt(pos.y),
+                edge
+            );
         }
     }
     mousedElement.setAttribute("cx", pos.x);
